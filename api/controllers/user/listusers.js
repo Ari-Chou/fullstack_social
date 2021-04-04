e608e833a4fb07e098ba66090118af1386b9b094
AriChou <46300506+Ari-Chou@users.noreply.github.com>
2021-04-04 22:38:45 +0800

    use custom JSON method to define the data of send back

---
+++ api/controllers/user/listusers.js
@@ -0,0 +1,10 @@
+module.exports = async function (req, res) {
+    const users = await User.find({ id: "1" })
+    const objc = []
+
+    // Splic user info
+    users.forEach(user => {
+        objc.push({ id: user.id, fullName: user.fullName, email: user.emailAddress})
+    });
+    res.send(users)
+}
---
+++ config/session.js
@@ -0,0 +1,17 @@
+/**
+ * Session Configuration
+ * (sails.config.session)
+ *
+ * Use the settings below to configure session integration in your app.
+ * (for additional recommended settings, see `config/env/production.js`)
+ *
+ * For all available options, see:
+ * https://sailsjs.com/config/session
+ */
+
+module.exports.session = {
+  secret: "0304f6ca28a4d8c0383b05575b27a068",
+  adapter: "connect-mongo",
+  url: "mongodb://fullstack:qezjIr-jyqber-7hiwbo@cluster0-shard-00-00.osyqc.mongodb.net:27017,cluster0-shard-00-01.osyqc.mongodb.net:27017,cluster0-shard-00-02.osyqc.mongodb.net:27017/fullstack?ssl=true&replicaSet=atlas-a0zv4i-shard-0&authSource=admin&retryWrites=true&w=majority",
+  collection: "sessions",
+};
