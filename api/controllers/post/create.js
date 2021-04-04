366b16ce07f36fa9fb64130e753e8f0b1182e357
AriChou <46300506+Ari-Chou@users.noreply.github.com>
2021-04-05 00:08:56 +0800

    create post and fecth

--- api/controllers/post/create.js
+++ api/controllers/post/create.js
@@ -1,4 +1,8 @@
-module.exports = function (req, res) {
-  const postBody = req.body.PostBody;
+module.exports = async function (req, res) {
+  const postBody = req.body.PostBody; // Get the input value
+  await Post.create({ text: postBody }); // Create Post
+
+  const posts = await Post.find({}); // Get the Posts
+  res.send(posts); // Send back to browser
   console.log("create a new Post:", postBody);
 };
