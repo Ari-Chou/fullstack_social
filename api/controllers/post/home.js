a26edcf74d8fa9549e232fffbb8e8f4278ca54b3
AriChou <46300506+Ari-Chou@users.noreply.github.com>
2021-04-08 00:59:21 +0800

    use JSON to custom expose data

--- api/controllers/post/home.js
+++ api/controllers/post/home.js
@@ -8,12 +8,18 @@ module.exports = async function (req, res) {
       res.send(allPosts)
   }
 
-  allPosts.forEach(p => {
-    p.user = {id: p.user.id, fullName: p.user.fullName}
-  });
-  
+  // allPosts.forEach(p => {
+  //   p.user = {id: p.user.id, fullName: p.user.fullName}
+  // });
+
+  // use this function to custom exposed data
+  // why we can use this to custom exposed data, because we will call the User Model customToJSON()
+  const string = JSON.stringify(allPosts)
+  const objects = JSON.parse(string) 
+
+
   res.view("pages/post/home", {
-    allPosts,
+    allPosts: objects,
     layout: "layouts/nav-layout"
   });
 }; 
